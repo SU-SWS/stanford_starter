cced31942d4ab48ed462e54b73ac912c13451232
Shea McKinney <sherakama@gmail.com>
2019-03-07 21:17:51 -0800

    Initial Commit.

---
+++ Gruntfile.js
@@ -0,0 +1,122 @@
+/**
+ * @file
+ */
+module.exports = function(grunt) {
+
+  // This is where we configure each task that we'd like to run.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      // This is where we set up all the tasks we'd like grunt to watch for changes.
+      scripts: {
+        files: ['js/source/**/*.js'],
+        tasks: ['uglify'],
+        options: {
+          spawn: false,
+        },
+      },
+      css: {
+        files: ['scss/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          interrupt: true
+        }
+      },
+      twig: {
+        files: ['templates/**/*.html.twig'],
+        tasks: ['uglify', 'svgmin', 'imagemin', 'sass']
+      }
+    },
+    uglify: {
+      // This is for minifying all of our scripts.
+      options: {
+        sourceMap: true,
+        mangle: false
+      },
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'js/source',
+          src: '{,*/}*.js',
+          dest: 'js/build'
+        }]
+      }
+    },
+    sass: {
+      // This will compile all of our sass files
+      // Additional configuration options can be found at https://github.com/sindresorhus/grunt-sass
+      options: {
+        includePaths: [
+          "scss",
+          "node_modules/bourbon/core",
+          "node_modules"
+        ],
+        sourceMap: true,
+        // This controls the compiled css and can be changed to nested, compact or compressed.
+        outputStyle: 'nested',
+        precision: 10
+      },
+      dist: {
+        files: {
+          'css/base/base.css':              'scss/base/index.scss',
+          'css/components/components.css':  'scss/components/index.scss',
+          'css/layout/layout.css':          'scss/layout/index.scss',
+          'css/states/states.css':          'scss/states/index.scss',
+          'css/theme/theme.css':            'scss/theme/index.scss',
+          'css/print/print.css':            'scss/print/index.scss'
+        }
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : [
+            'css/**/*.css',
+            'templates/**/*.twig',
+            'images/optimized/**/*.{png,jpg,gif,svg}',
+            'js/build/**/*.js',
+            '*.theme'
+          ]
+        },
+        options: {
+          watchTask: true,
+          // reloadDelay: 1000,
+          // reloadDebounce: 500,
+          reloadOnRestart: true,
+          logConnections: true,
+          injectChanges: false // Depends on enabling the link_css module
+        }
+      }
+    },
+    availabletasks: {
+      tasks: {
+        options: {
+          filter: "include",
+          tasks: [
+            'browserSync',
+            'sass',
+            'uglify',
+            'watch',
+            'devmode'
+          ]
+        }
+      }
+    }
+  });
+
+  // This is where we tell Grunt we plan to use this plug-in.
+  grunt.loadNpmTasks('grunt-git');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-available-tasks');
+
+  // My tasks.
+  grunt.registerTask('devmode', "Watch and BrowserSync all in one.", ['browserSync', 'watch']);
+
+  // This is where we tell Grunt what to do when we type "grunt" into the terminal.
+  // Note: if you'd like to run and of the tasks individually you can do so by typing 'grunt mytaskname' alternatively
+  // you can type 'grunt watch' to automatically track your files for changes.
+  grunt.registerTask('default', ['availabletasks']);
+};
---
+++ README.md
@@ -0,0 +1,37 @@
+# [Stanford Starter](https://github.com/SU-SWS/stanford_starter)
+##### Version: 8.x-1.0-dev
+
+Changelog: [Changelog.txt](CHANGELOG.txt)
+
+Description
+---
+
+Stanford Starter is a Stanford sub-theme that works with the Stanford Basic theme.
+
+Installation
+---
+
+Copy this theme and change all files, folders, and names from stanford_starter to the name of your theme.
+
+Configuration
+---
+
+Nothing special needed. Install, enable, and set as the default active theme.
+
+Developer
+---
+
+If you wish to develop on this theme you will most likely need to compile some new css. Please use the sass structure provided and compile with the sass compiler packaged in this theme. To install:
+
+```
+npm install
+grunt watch
+grunt sass
+ or
+grunt devmode
+```
+
+Contribution / Collaboration
+---
+
+You are welcome to contribute functionality, bug fixes, or documentation to this theme. If you would like to suggest a fix or new functionality you may add a new issue to the GitHub issue queue or you may fork this repository and submit a pull request. For more help please see [GitHub's article on fork, branch, and pull requests](https://help.github.com/articles/using-pull-requests)
