9ec9d8b8c1f5c5947c228d55ac64f46b190f5153
Katria Lesser <Katria@stanford.edu>
2020-05-08 09:26:12 -0600

    ADAPT-283: Reorganized file structure to src and dist

--- js/build/scripts.js
+++
@@ -1,2 +0,0 @@
-!function(Drupal,$,window){Drupal.behaviors.stanford_starter={attach:function(context,settings){$(window).load(function(){}),$(window).resize(function(){}),$(window).scroll(function(){})}}}(Drupal,jQuery,this);
-//# sourceMappingURL=scripts.js.map
